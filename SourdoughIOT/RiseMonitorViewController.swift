5883653e66b8c62f4743ecb179c0ccc239c64cbb
walsha61 <walsha61@tcd.ie>
2022-03-12 22:09:13 +0000

    Added navigation control for two views

---
+++ SourdoughIOT/RiseMonitorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RiseMonitorViewController.swift
+//  SourdoughIOT
+//
+//  Created by Anneliese Walsh on 12/03/2022.
+//  Copyright © 2022 Anneliese Walsh. All rights reserved.
+//
+
+import UIKit
+
+class RiseMonitorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
